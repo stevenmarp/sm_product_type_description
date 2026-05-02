bed2c7158127c2d3434d822ccfd941e2ebec2ca8
Steven Marp <stevenmarp@users.noreply.github.com>
2026-05-02 21:25:32 +0700

    Bump module version for Odoo 19

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Product Type Description",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Inventory/Inventory",
     "summary": "Searchable type description field on products",
     "description": """
